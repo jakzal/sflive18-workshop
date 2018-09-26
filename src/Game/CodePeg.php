ce2234179082c03cd89965c675b3fe3813ff4d7d
Jakub Zalas <jakub@zalas.pl>
2018-09-26 11:19:02 +0100

    [2] (CodePeg) Make the test pass
    in order to fix the following failure:
    
    ...F                                                                4 / 4 (100%)
    
    Time: 33 ms, Memory: 4.00MB
    
    There was 1 failure:
    
    1) App\Tests\Game\CodePegTest::test_it_throws_an_unknown_colour_exception_if_initialized_with_an_unsupported_colour
    Failed asserting that exception of type "App\Game\UnknownColourException" is thrown.

--- src/Game/CodePeg.php
+++ src/Game/CodePeg.php
@@ -4,10 +4,16 @@ namespace App\Game;
 
 class CodePeg
 {
+    private const AVAILABLE_COLOURS = ['Red', 'Green', 'Blue', 'Yellow', 'Purple'];
+
     private $colour;
 
     public function __construct(string $colour)
     {
+        if (!in_array($colour, self::AVAILABLE_COLOURS)) {
+            throw new UnknownColourException($colour, self::AVAILABLE_COLOURS);
+        }
+
         $this->colour = $colour;
     }
 
