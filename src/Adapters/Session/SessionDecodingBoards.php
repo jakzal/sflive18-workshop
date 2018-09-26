45849af3fa55f34756441bdfcef88954fa4d89d5
Jakub Zalas <jakub@zalas.pl>
2018-09-26 15:16:19 +0100

    [4] (SessionDecodingBoards) Make the test pass

--- src/Adapters/Session/SessionDecodingBoards.php
+++ src/Adapters/Session/SessionDecodingBoards.php
@@ -6,18 +6,33 @@ use App\Game\DecodingBoard;
 use App\Game\DecodingBoardNotFoundException;
 use App\Game\DecodingBoards;
 use App\Game\GameUuid;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 final class SessionDecodingBoards implements DecodingBoards
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @throws DecodingBoardNotFoundException
      */
     public function get(GameUuid $uuid): DecodingBoard
     {
-        throw new DecodingBoardNotFoundException($uuid);
+        $board = $this->session->get((string) $uuid);
+
+        if (!$board instanceof DecodingBoard) {
+            throw new DecodingBoardNotFoundException($uuid);
+        }
+
+        return $board;
     }
 
     public function put(DecodingBoard $decodingBoard)
     {
+        $this->session->set((string) $decodingBoard->gameUuid(), $decodingBoard);
     }
 }
