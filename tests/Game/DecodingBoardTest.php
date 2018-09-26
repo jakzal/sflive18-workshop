7948fc6fc3bdeb4bb4a5707c55df75e67cec6e7b
Jakub Zalas <jakub@zalas.pl>
2018-09-26 14:01:42 +0100

    [3] (DecodingBoard) Refactor - extract common test code into the setUp method before adding a new test case

--- tests/Game/DecodingBoardTest.php
+++ tests/Game/DecodingBoardTest.php
@@ -7,22 +7,55 @@ use App\Game\DecodingBoard;
 use App\Game\Feedback;
 use App\Game\GameUuid;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class DecodingBoardTest extends TestCase
 {
     const NUMBER_OF_ATTEMPTS = 8;
 
+    /**
+     * @var DecodingBoard
+     */
+    private $board;
+
+    /**
+     * @var GameUuid
+     */
+    private $uuid;
+
+    /**
+     * @var Feedback
+     */
+    private $feedback;
+
+    /**
+     * @var Code|ObjectProphecy
+     */
+    private $guessCode;
+
+    /**
+     * @var Code|ObjectProphecy
+     */
+    private $secretCode;
+
+    protected function setUp()
+    {
+        $this->uuid = GameUuid::existing('547bf8e4-1a9c-492e-a0cf-165b809585a2');
+        $this->feedback = $this->prophesize(Feedback::class)->reveal();
+        $this->guessCode = $this->prophesize(Code::class);
+        $this->secretCode = $this->prophesize(Code::class);
+
+        $this->secretCode->match($this->guessCode)->willReturn($this->feedback);
+
+        $this->board = new DecodingBoard($this->uuid, $this->secretCode->reveal(), self::NUMBER_OF_ATTEMPTS);
+    }
+
     public function test_makeGuess_gives_feedback_on_the_guess()
     {
-        $uuid = GameUuid::existing('547bf8e4-1a9c-492e-a0cf-165b809585a2');
-        $expectedFeedback = $this->prophesize(Feedback::class)->reveal();
-        $guessCode = $this->prophesize(Code::class);
-        $secretCode = $this->prophesize(Code::class);
-        $secretCode->match($guessCode)->willReturn($expectedFeedback);
+        $this->secretCode->match($this->guessCode)->willReturn($this->feedback);
 
-        $board = new DecodingBoard($uuid, $secretCode->reveal(), self::NUMBER_OF_ATTEMPTS);
-        $feedback = $board->makeGuess($guessCode->reveal());
+        $feedback = $this->board->makeGuess($this->guessCode->reveal());
 
-        $this->assertSame($expectedFeedback, $feedback);
+        $this->assertSame($this->feedback, $feedback);
     }
 }
