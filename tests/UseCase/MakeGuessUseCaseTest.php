18fd72b7c0ccf99ca46d17db1e4d0124a692283e
Jakub Zalas <jakub@zalas.pl>
2018-09-26 14:59:50 +0100

    [3] (MakeGuessUseCase) Define a test case - it puts the board into the repository after making a guess

--- tests/UseCase/MakeGuessUseCaseTest.php
+++ tests/UseCase/MakeGuessUseCaseTest.php
@@ -74,6 +74,19 @@ class MakeGuessUseCaseTest extends TestCase
         $this->assertSame($this->board->reveal(), $returnedBoard);
     }
 
+
+    public function test_it_puts_the_board_into_repository_after_making_a_guess()
+    {
+        $code = $this->code();
+
+        $this->board->makeGuess($code)->willReturn($this->feedback);
+        $this->boards->get($this->uuid)->willReturn($this->board);
+
+        $this->useCase->execute($this->uuid, $code);
+
+        $this->boards->put($this->board)->shouldHaveBeenCalled();
+    }
+
     private function code(): Code
     {
         return $this->prophesize(Code::class)->reveal();
