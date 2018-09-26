63253d3ed02f4aa8a388f18d7102acd7badd137f
Jakub Zalas <jakub@zalas.pl>
2018-09-26 14:58:40 +0100

    [3] (MakeGuessUseCase) Make the test pass

---
+++ src/UseCase/MakeGuessUseCase.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\UseCase;
+
+use App\Game\Code;
+use App\Game\DecodingBoards;
+use App\Game\GameUuid;
+
+class MakeGuessUseCase
+{
+    private $boards;
+
+    public function __construct(DecodingBoards $boards)
+    {
+        $this->boards = $boards;
+    }
+
+    public function execute(GameUuid $uuid, Code $guessCode)
+    {
+        $board = $this->boards->get($uuid);
+        $board->makeGuess($guessCode);
+    }
+}
