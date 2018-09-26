32fa72328c1c25ae962bc4e65438a765a9fb826a
Jakub Zalas <jakub@zalas.pl>
2018-09-26 11:42:16 +0100

    [2] (Code) Define a test case - it is created from a string of code pegs

---
+++ tests/Game/CodeTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Game;
+
+use App\Game\Code;
+use PHPUnit\Framework\TestCase;
+
+class CodeTest extends TestCase
+{
+    public function test_it_is_created_from_a_string_of_code_pegs()
+    {
+        $code = Code::fromString('Red Green Blue');
+
+        $this->assertInstanceOf(Code::class, $code);
+        $this->assertEquals(
+            [
+                new CodePeg('Red'),
+                new CodePeg('Green'),
+                new CodePeg('Blue'),
+            ],
+            $code->pegs()
+        );
+    }
+}
