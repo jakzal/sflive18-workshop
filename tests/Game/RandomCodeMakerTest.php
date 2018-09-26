432c0ef0e89ca4c8e8a891870d718df131df1981
Jakub Zalas <jakub@zalas.pl>
2018-09-26 15:31:10 +0100

    [4] (InMemoryCodeMaker) Extract the code maker test case

--- tests/Game/RandomCodeMakerTest.php
+++ tests/Game/RandomCodeMakerTest.php
@@ -2,33 +2,13 @@
 
 namespace App\Tests\Game;
 
-use App\Game\Code;
 use App\Game\CodeMaker;
 use App\Game\RandomCodeMaker;
-use PHPUnit\Framework\TestCase;
 
-class RandomCodeMakerTest extends TestCase
+class RandomCodeMakerTest extends CodeMakerTestCase
 {
-    /**
-     * @var CodeMaker
-     */
-    protected $codeMaker;
-
-    protected function setUp()
+    protected function createCodeMaker(): CodeMaker
     {
-        $this->codeMaker = new RandomCodeMaker();
-    }
-
-    public function test_it_is_a_code_maker()
-    {
-        $this->assertInstanceOf(CodeMaker::class, $this->codeMaker);
-    }
-
-    public function test_it_returns_a_code_of_given_length()
-    {
-        $code = $this->codeMaker->newCode(3);
-
-        $this->assertInstanceOf(Code::class, $code);
-        $this->assertSame(3, $code->length());
+        return new RandomCodeMaker();
     }
 }
