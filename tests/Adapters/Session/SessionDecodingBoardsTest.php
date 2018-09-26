0eb3ee808b76d6d141adcd581259554f17682dfc
Jakub Zalas <jakub@zalas.pl>
2018-09-26 15:12:59 +0100

    [4] (SessionDecodingBoards) Define a test case - it throws an exception if no board was found

--- tests/Adapters/Session/SessionDecodingBoardsTest.php
+++ tests/Adapters/Session/SessionDecodingBoardsTest.php
@@ -3,13 +3,41 @@
 namespace App\Tests\Adapters\Session;
 
 use App\Adapters\Session\SessionDecodingBoards;
+use App\Game\Code;
+use App\Game\DecodingBoard;
+use App\Game\DecodingBoardNotFoundException;
 use App\Game\DecodingBoards;
+use App\Game\GameUuid;
 use PHPUnit\Framework\TestCase;
 
 class SessionDecodingBoardsTest extends TestCase
 {
+    private const UUID = 'd97f4f5d-2f38-459e-a08d-3b950200ba25';
+    private const INVALID_UUID = '027808e1-7f0f-46d0-8b68-9b69eb100c4d';
+
+    /**
+     * @var DecodingBoards
+     */
+    private $decodingBoards;
+
+    protected function setUp()
+    {
+        $this->decodingBoards = new SessionDecodingBoards();
+    }
+
     public function test_it_is_a_decoding_boards_repository()
     {
-        $this->assertInstanceOf(DecodingBoards::class, new SessionDecodingBoards());
+        $this->assertInstanceOf(DecodingBoards::class, $this->decodingBoards);
+    }
+
+    public function test_it_throws_no_board_found_exception_if_no_board_was_found()
+    {
+        $this->expectException(DecodingBoardNotFoundException::class);
+
+        $uuid = GameUuid::existing(self::INVALID_UUID);
+        $board = new DecodingBoard($uuid, Code::fromString('Red Blue'), 12);
+
+        $this->decodingBoards->put($board);
+        $this->decodingBoards->get(GameUuid::existing(self::UUID));
     }
 }
