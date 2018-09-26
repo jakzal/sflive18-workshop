d86e306c6ad1b252cc0ae95039f7df7eb563a612
Jakub Zalas <jakub@zalas.pl>
2018-09-26 14:57:17 +0100

    [3] (MakeGuessUseCase) Define a test case - it makes a guess

---
+++ tests/UseCase/MakeGuessUseCaseTest.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\UseCase;
+
+use App\Game\Code;
+use App\Game\DecodingBoard;
+use App\Game\DecodingBoards;
+use App\Game\Feedback;
+use App\Game\GameUuid;
+use App\UseCase\MakeGuessUseCase;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class MakeGuessUseCaseTest extends TestCase
+{
+    const GAME_UUID = '32915568-55ac-48d1-b817-9d8fa9cdff6b';
+
+    /**
+     * @var MakeGuessUseCase
+     */
+    private $useCase;
+
+    /**
+     * @var DecodingBoards|ObjectProphecy
+     */
+    private $boards;
+
+    /**
+     * @var DecodingBoard|ObjectProphecy
+     */
+    private $board;
+
+    /**
+     * @var Feedback|ObjectProphecy
+     */
+    private $feedback;
+
+    /**
+     * @var GameUuid
+     */
+    private $uuid;
+
+    protected function setUp()
+    {
+        $this->board = $this->prophesize(DecodingBoard::class);
+        $this->uuid = GameUuid::existing(self::GAME_UUID);
+        $this->feedback = $this->prophesize(Feedback::class);
+        $this->boards = $this->prophesize(DecodingBoards::class);
+
+        $this->useCase = new MakeGuessUseCase($this->boards->reveal());
+    }
+
+    public function test_it_makes_a_guess()
+    {
+        $code = $this->code();
+
+        $this->board->makeGuess($code)->willReturn($this->feedback);
+        $this->boards->get($this->uuid)->willReturn($this->board);
+
+        $this->useCase->execute($this->uuid, $code);
+
+        $this->board->makeGuess($code)->shouldHaveBeenCalled();
+    }
+
+    private function code(): Code
+    {
+        return $this->prophesize(Code::class)->reveal();
+    }
+}
