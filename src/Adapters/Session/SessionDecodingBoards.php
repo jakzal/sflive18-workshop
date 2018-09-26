8cd4740ef7576c7c41ae0bf1ef385cbb130cda78
Jakub Zalas <jakub@zalas.pl>
2018-09-26 15:09:50 +0100

    [4] (SessionDecodingBoards) Make the test pass

---
+++ src/Adapters/Session/SessionDecodingBoards.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Adapters\Session;
+
+use App\Game\DecodingBoard;
+use App\Game\DecodingBoardNotFoundException;
+use App\Game\DecodingBoards;
+use App\Game\GameUuid;
+
+final class SessionDecodingBoards implements DecodingBoards
+{
+    /**
+     * @throws DecodingBoardNotFoundException
+     */
+    public function get(GameUuid $uuid): DecodingBoard
+    {
+    }
+
+    public function put(DecodingBoard $decodingBoard)
+    {
+    }
+}
