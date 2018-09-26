6f245799fcc1984510820c0346585bda78756497
Jakub Zalas <jakub@zalas.pl>
2018-09-26 15:22:00 +0100

    [4] (RandomCodeMaker) Define a test case - it returns a code of given length

--- tests/Game/RandomCodeMakerTest.php
+++ tests/Game/RandomCodeMakerTest.php
@@ -2,14 +2,33 @@
 
 namespace App\Tests\Game;
 
+use App\Game\Code;
 use App\Game\CodeMaker;
 use App\Game\RandomCodeMaker;
 use PHPUnit\Framework\TestCase;
 
 class RandomCodeMakerTest extends TestCase
 {
+    /**
+     * @var CodeMaker
+     */
+    protected $codeMaker;
+
+    protected function setUp()
+    {
+        $this->codeMaker = new RandomCodeMaker();
+    }
+
     public function test_it_is_a_code_maker()
     {
-        $this->assertInstanceOf(CodeMaker::class, new RandomCodeMaker());
+        $this->assertInstanceOf(CodeMaker::class, $this->codeMaker);
+    }
+
+    public function test_it_returns_a_code_of_given_length()
+    {
+        $code = $this->codeMaker->newCode(3);
+
+        $this->assertInstanceOf(Code::class, $code);
+        $this->assertSame(3, $code->length());
     }
 }
