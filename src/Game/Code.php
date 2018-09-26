7e9792642b973ca8f930f91ff54f8cfb801ed4c5
Jakub Zalas <jakub@zalas.pl>
2018-09-26 11:48:24 +0100

    [2] (Code) Make the test pass
    in order to remove the following failure:
    
    ....F.                                                              6 / 6 (100%)
    
    Time: 41 ms, Memory: 4.00MB
    
    There was 1 failure:
    
    1) App\Tests\Game\CodeTest::test_it_is_created_from_a_string_of_code_pegs
    Failed asserting that two arrays are equal.
    --- Expected
    +++ Actual
    @@ @@
     Array (
    -    0 => App\Game\CodePeg Object (...)
    -    1 => App\Game\CodePeg Object (...)
    -    2 => App\Game\CodePeg Object (...)
     )
    
    /mastermind/tests/Game/CodeTest.php:22

--- src/Game/Code.php
+++ src/Game/Code.php
@@ -4,13 +4,30 @@ namespace App\Game;
 
 class Code
 {
+    private $codePegs;
+
+    /**
+     * @param CodePeg[] $codePegs
+     */
+    private function __construct(array $codePegs)
+    {
+        $this->codePegs = $codePegs;
+    }
+
     public static function fromString(string $string)
     {
-        return new Code();
+        return new Code(
+            array_map(
+                function (string $colour) {
+                    return new CodePeg($colour);
+                },
+                explode(' ', $string)
+            )
+        );
     }
 
     public function pegs()
     {
-        return [];
+        return $this->codePegs;
     }
 }
